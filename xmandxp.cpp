b0c588c82ca86e143a3e478dcba4c130511b14dc
dailing57 <dailing57@163.com>
2021-06-23 23:17:58 +0800

    get the correspondences between XM and XP derived by LOC

---
+++ xmandxp.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <regex>
+#include <fstream>
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    regex xm(R"(XM_.*?[\n;])");
+    regex xp(R"(XP_.*?[\n;])");
+    smatch resm, resp;
+    cout<<"running"<<endl;
+    freopen("Pop_tri.txt", "r", stdin);
+    freopen("T0.txt", "w", stdout);
+    string line;
+    set < pair<string, string>> st;
+    while (getline(cin, line))
+    {
+        //cout<<"running"<<endl;
+        if (regex_search(line, resm, xm) && regex_search(line, resp, xp))
+        {
+            st.insert({ resm[0],resp[0] });
+        }
+    }
+    for (auto x : st)
+    {
+        cout << x.first << "\t" << x.second << endl;
+    }
+    return 0;
+}
+
